c137954b7fedbacd99bc6d65662c27a0627aea69
Gustav Bertram <gustav.bertram@gmail.com>
2018-03-01 23:15:02 +0200

    Add Ch 2.7 from On Lisp

---
+++ on_lisp/2_7_local_functions.lisp
@@ -0,0 +1,9 @@
+(defun count-instances (obj lsts)
+  (labels ((instances-in (lst)
+             (if (consp lst)
+                 (+ (if (eq (car lst) obj) 1 0)
+                    (instances-in (cdr lst)))
+                 0)))
+    (mapcar #'instances-in lsts)))
+
+(consp 'a)
