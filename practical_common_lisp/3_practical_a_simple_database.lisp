1dba501d8c07b48681b5f996e6221f46b8bc7fb9
Gustav Bertram <gustav.bertram@gmail.com>
2018-03-05 17:58:49 +0200

    Add DB saving to PCL example

--- practical_common_lisp/3_practical_a_simple_database.lisp
+++ practical_common_lisp/3_practical_a_simple_database.lisp
@@ -9,27 +9,43 @@
   (push cd *db*))
 
 (defun dump-db ()
-  "Dump the db"
+  "Dump the db."
   (dolist (cd *db*)
     (format t "~{~a:~10t~a~%~}~%" cd)))
 
 (defun dump-db1 ()
-  "Alternate DB dump"
+  "Alternate DB dump."
   (format t "~{~{~a:~10t~a~%~}~%~}" *db*))
 
 (defun prompt-read (prompt)
-  "Read typed response to a prompt"
+  "Read typed response to a prompt."
   (format *query-io* "~a: " prompt)
   (force-output *query-io*)
   (read-line *query-io*))
 
 (defun prompt-for-cd ()
-  "Make a CD from interactive prompts"
+  "Make a CD from interactive prompts."
   (make-cd
    (prompt-read "Title")
    (prompt-read "Artist")
-   (prompt-read "Rating")
-   (prompt-read "Ripped [y/n]")))
+   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+   (y-or-n-p "Ripped [y/n]")))
 
+(defun add-cds ()
+  "Add a bunch of CDs."
+  (loop (add-record (prompt-for-cd))
+        (if (not (y-or-n-p "Another? [y/n]: ")) (return))))
 
+(defun save-db (&optional (filename "~/pcl_db.lisp"))
+  "Save the DB."
+  (with-open-file (out filename
+                       :direction :output
+                       :if-exists :supersede)
+    (with-standard-io-syntax
+      (print *db* out))))
 
+(defun load-db (&optional (filename "~/pcl_db.lisp"))
+  "Load the DB."
+  (with-open-file (in filename)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
