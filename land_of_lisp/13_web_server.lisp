946d69ebe237197fa8c2a49c801d1907f385fc99
Gustav Bertram <gustav.bertram@gmail.com>
2018-03-17 23:44:36 +0200

    Adding part of LOL Ch 13

---
+++ land_of_lisp/13_web_server.lisp
@@ -0,0 +1,26 @@
+;;;; WEB SERVER
+
+;;; Errors
+(error "foo")
+
+(define-condition foo () ()
+  (:report (lambda (condition stream)
+             (princ "Stop FOOing around, numbskull!" stream))))
+(error 'foo)
+
+;; Intercepting conditions
+
+(defun bad-function ()
+  (error 'foo))
+
+(handler-case (bad-function)
+  (foo () "somebody signaled foo!")
+  (bar () "somebody signaled bar!"))
+
+(unwind-protect (/ 1 0)
+  (princ "I need to say 'flubyduby' matter what"))
+
+
+
+
+
