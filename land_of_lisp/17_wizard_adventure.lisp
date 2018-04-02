b71816a9a8ff0005be78262ace907d130829040b
Gustav Bertram <gustav.bertram@gmail.com>
2018-04-02 23:46:02 +0200

    Add two new commands to Wizard Adventure Game for Land of Lisp Ch 17

--- land_of_lisp/17_wizard_adventure.lisp
+++ land_of_lisp/17_wizard_adventure.lisp
@@ -122,3 +122,45 @@
     (unless (eq (car cmd) 'quit)
       (game-print (game-eval cmd))
       (game-repl))))
+
+;;;; Adding a DSL
+
+;;; Create two commands by hand to see what they have in common
+
+;; Weld
+(defun have (object)
+  (member object (inventory)))
+
+(defparameter *chain-welded* nil)
+
+(defun weld (subject object)
+  (if (and (eq *location* 'attic)
+           (eq subject 'chain)
+           (eq object 'bucket)
+           (have 'chain)
+           (have 'bucket)
+           (not *chain-welded*))
+      (progn (setf *chain-welded* t)
+             '(the chain is now securely welded to the bucket.))
+      '(you cannot weld like that.)))
+
+(pushnew 'weld *allowed-commands*)
+
+;; Dunk
+
+(defparameter *bucket-filled* nil)
+
+(defun dunk (subject object)
+  (if (and (eq *location* 'garden)
+           (eq subject 'bucket)
+           (eq object 'well)
+           (have 'bucket)
+           Domain-Specific Languages 369
+           *chain-welded*)
+      (progn (setf *bucket-filled* 't)
+             '(the bucket is now full of water))
+      '(you cannot dunk like that.)))
+
+(pushnew 'dunk *allowed-commands*)
+
+
