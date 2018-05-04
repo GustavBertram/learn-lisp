a474183a3bd7a50f733ff8d2ee3131a67fe245cb
Gustav Bertram <gustav.bertram@gmail.com>
2018-05-04 23:54:01 +0200

    Added more to Let over Lambda Ch 2

--- let-over-lambda/2-closures.lisp
+++ let-over-lambda/2-closures.lisp
@@ -14,3 +14,19 @@
 
 (let ((temp-special 2))
   (temp-special-returner))
+
+;;; Lambda
+
+;; (function '(lambda (x) (+ 1 x))) ; Doesn't actually work
+(function (lambda (x) (+ 1 x)))
+
+;; Equivalent to:
+#'(lambda (x) (+ 1 x))
+
+;; Equivalent to:
+(lambda (x) (+ 1 x))
+
+;; Can't call a function to dynamically retirn the symbol to be used in a regular function call (why not?)
+;; Use FUNCALL or APPLY for this.
+
+
