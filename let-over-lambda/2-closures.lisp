3903c6794db4aae03505453880f769fe50812794
Gustav Bertram <gustav.bertram@gmail.com>
2018-05-05 23:20:07 +0200

    Added Let Over Lambda, Ch 2

--- let-over-lambda/2-closures.lisp
+++ let-over-lambda/2-closures.lisp
@@ -31,7 +31,7 @@
 
 ((lambda (x) (+ 1 x)) 2)
 
-;;; Lambda folding
+;;; Lambda folding -------------------------------------------------------------
 
 (defun compiler-test ()
   (funcall
@@ -45,17 +45,78 @@
 
 (funcall (lambda-returner) 2)
 
-(defun let-over-lambda-returner ()
-  (let ((y 1)) ; Creates a new environment
+(defun let-over-lambda-returner (z)
+  (let ((y z)) ; Creates a new environment
     (lambda (x)
       (incf y x))))
 
-(funcall (let-over-lambda-returner) 2)
+(funcall (let-over-lambda-returner 3) 2)
 
 (progn
   (compile 'let-over-lambda-returner)
-  (time (let-over-lambda-returner))) ; 0 bytes consed
+  (time (let-over-lambda-returner 4))) ; 0 bytes consed?
 
-(compile 'let-over-lambda-returner) ; OK, this is not supposed to work
+;;; Let over lambda ------------------------------------------------------------
+
+(let ((x 0))
+  (lambda () x))
+
+(let ((counter 0))
+  (values
+   (lambda () (incf counter))
+   (lambda () (decf counter))))
+
+;;; Lambda over let over lambda
+
+;; Lambda expressions are constants, pointers to machine code
+(lambda ()
+  (let ((counter 0))
+    (lambda () (incf counter))))
+
+(defun make-counter () ; Data attached to a function without an object
+  (let ((counter 0))
+    (lambda () (incf counter)))) 
+
+;; Non-trivial example of function with attached data
+(defun block-scanner (trigger-string)
+  (let* ((trig (coerce trigger-string 'list))
+         (curr trig))
+    (lambda (data-string)
+      (let ((data (coerce data-string 'list)))
+        (dolist (c data)
+          (if curr
+              (setq curr
+                    (if (char= (car curr) c)
+                        (cdr curr)      ; next char
+                        trig))))        ; start over 
+        (not curr)))))                  ; return t if found
+
+(defvar scanner
+  (block-scanner "test"))
+
+(funcall scanner "We will start ") ; NIL
+(funcall scanner "the te")         ; NIL
+(funcall scanner "st tomorrow.")   ; T
+
+;;; Let over lambda over let over lambda
+
+;; A way to implement class variables, or static variables
+(let ((direction 'up))
+  (defun toggle-counter-direction ()
+    (setq direction
+          (if (eq direction 'up)
+              'down
+              'up)))
+
+  (defun counter-class ()
+    (let ((counter 0))
+      (lambda ()
+        (if (eq direction 'up)
+            (incf counter)
+            (decf counter))))))
+
+
+
+
 
 
