d599385a7f37cbd67dd61f04d5c4a183db45e5f7
Gustav Bertram <gustav.bertram@gmail.com>
2018-05-05 21:53:35 +0200

    Adding to Let over Lambda Ch 2

--- let-over-lambda/2-closures.lisp
+++ let-over-lambda/2-closures.lisp
@@ -29,4 +29,33 @@
 ;; Can't call a function to dynamically retirn the symbol to be used in a regular function call (why not?)
 ;; Use FUNCALL or APPLY for this.
 
+((lambda (x) (+ 1 x)) 2)
+
+;;; Lambda folding
+
+(defun compiler-test ()
+  (funcall
+    (lambda (x) (+ 1 x))
+    2))
+
+;; (disassemble 'compiler-test)
+
+(defun lambda-returner ()
+  (lambda (x) (1+ x))) ; No overhead
+
+(funcall (lambda-returner) 2)
+
+(defun let-over-lambda-returner ()
+  (let ((y 1)) ; Creates a new environment
+    (lambda (x)
+      (incf y x))))
+
+(funcall (let-over-lambda-returner) 2)
+
+(progn
+  (compile 'let-over-lambda-returner)
+  (time (let-over-lambda-returner))) ; 0 bytes consed
+
+(compile 'let-over-lambda-returner) ; OK, this is not supposed to work
+
 
