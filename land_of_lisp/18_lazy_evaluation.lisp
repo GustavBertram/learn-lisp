015b635c30b37fc21f3db8808a1796037977dd98
Gustav Bertram <gustav.bertram@gmail.com>
2018-05-08 21:03:26 +0200

    Adding to Land of Lisp, Ch 18

--- land_of_lisp/18_lazy_evaluation.lisp
+++ land_of_lisp/18_lazy_evaluation.lisp
@@ -1,4 +1,5 @@
 ;;;; LAZY EVALUATION
+;;; Contains code from Land of Lisp Ch 18
 
 (defmacro lazy (&body body)
   (let ((forced (gensym))
@@ -16,4 +17,60 @@
 
 ;; (lazy (+ 1 2))
 ;; (force (lazy (+ 1 2)))
+
+;;(defun add (a b)
+;;  (princ "I am adding now")
+;;  (+ a b))
+
+;; (defparameter *foo* (lazy (add 1 2)))
+;; (force *foo*)
+
+;;; Lazy Lists -----------------------------------------------------------------
+
+(defmacro lazy-cons (a d)
+  `(lazy (cons ,a ,d)))
+
+(defun lazy-car (x)
+  (car (force x)))
+
+(defun lazy-cdr (x)
+  (cdr (force x)))
+
+;; (defparameter *foo* (lazy-cons 4 7))
+;; (lazy-car *foo*)
+;; (lazy-cdr *foo*)
+
+(defparameter *integers*
+  (labels ((f (n)
+             (lazy-cons n (f (1+ n)))))
+    (f 1)))
+
+;; (lazy-car (lazy-cdr (lazy-cdr *integers*)))
+
+(defun lazy-nil ()
+  (lazy nil))
+
+(defun lazy-null (x)
+  (not (force x)))
+
+;;; Converting between lists and lazy lists ------------------------------------
+
+(defun make-lazy (lst)
+  (lazy (when lst
+          (cons (car lst) (make-lazy (cdr lst))))))
+
+(defun take (n lst)
+  (unless (or (zerop n) (lazy-null lst))
+    (cons (lazy-car lst) (take (1- n) (lazy-cdr lst)))))
+
+(defun take-all (lst)
+  (unless (lazy-null lst)
+    (cons (lazy-car lst) (take-all (lazy-cdr lst)))))
+
+;; (take 10 *integers*)
+;; (take 10 (make-lazy '(q w e r t y u i o p a s d f)))
+;; (take-all (make-lazy '(q w e r t y u i o p a s d f)))
+
+;;; Mapping and searching across lazy lists ------------------------------------
+
 
