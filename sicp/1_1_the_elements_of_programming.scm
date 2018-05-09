416935c3e353ac5e828f6f135ec31d5d4e9aa909
Gustav Bertram <gustav.bertram@gmail.com>
2018-05-09 09:56:39 +0200

    Adding SICP Ch 1.1 exercises

---
+++ sicp/1_1_the_elements_of_programming.scm
@@ -0,0 +1,21 @@
+#lang sicp
+
+;;;; 1.1 THE ELEMENTS OF PROGRAMMING
+;;;; Contains selected exercises and answers from SICP
+
+;;; Exercise 1.3.
+;; Define a procedure that takes three numbers as arguments and returns the sum
+;; of the squares of the two larger numbers. 
+
+(define (sum-sqr a b)
+  (+ (* a a)
+     (* b b)))
+
+(define (sum-sqr-of-two-largest a b c)
+  (cond ((and (<= a b) (<= a c)) (sum-sqr b c))
+        ((and (<= b a) (<= b c)) (sum-sqr a c))
+        ((and (<= c a) (<= c b)) (sum-sqr a b))))
+
+
+
+
