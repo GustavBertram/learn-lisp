aee4ac9a6ef7fd2a8d9a52c1aab41f22572efe46
Gustav Bertram <gustav.bertram@gmail.com>
2018-05-12 00:33:31 +0200

    Added rest of exercise 1.11 to SICP Ch 1.2

--- sicp/Ex1.11.scm
+++ sicp/Ex1.11.scm
@@ -8,7 +8,7 @@
 
 ;; Write a procedure that computes f by means of a recursive process.
 
-(define f (n)
+(define (f n)
   (if (< n 3)
       n
       (+ (f (- n 1))
@@ -17,3 +17,16 @@
 
 ;; Write a procedure that computes f by means of an iterative process. 
 
+(define (f-iter n)
+  (define (fi i a b c)
+    (if (< i n)
+        (fi (inc i) b c (+ c
+                           (* b 2)
+                           (* a 3)))
+        c))
+  
+  (if (< n 3)
+      n
+      (fi 2 0 1 2)))
+  
+  
