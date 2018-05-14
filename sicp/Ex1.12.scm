d7e19d44113158967ab81db87f7e4677abcc4509
Gustav Bertram <gustav.bertram@gmail.com>
2018-05-14 22:31:32 +0200

    Added single argument pascal's triangle function to Ex 1.12 in SICP

--- sicp/Ex1.12.scm
+++ sicp/Ex1.12.scm
@@ -24,3 +24,45 @@
         (else (+ (pascal (dec n) (dec m))
                  (pascal (dec n) m)))))
 
+; Need to know the outer edges to return 1 for them.
+
+;     0
+;    1 2
+;   3 4 5
+;  6 7 8 9
+
+; Left edge (0,1,3,6,...) are triangle numbers: https://oeis.org/A000217
+
+; Formula for triangle numbers: x = n(n+1)/2
+; Solved for n: n = (sqrt(8*x+1)-1)/2
+
+; When n is an integer, x is a triangle number.
+; If x+1 is a triangle number, it's on the other edge (0,2,5,9,...)
+
+(define (triangle-number? n)
+  (integer? (/ (- (sqrt (+ (* 8 n) 1) ) 1) 2)))
+
+; Need to get the row, to get to the parent numbers
+
+;   n: 0 1 2 3 4 5 6 7 8 9
+; row: 0 1 1 2 2 2 3 3 3 3
+
+; Matches this sequence: https://oeis.org/A003056
+; Formula: r = floor((sqrt(1+8*n)-1)/2)
+
+(define (pascal-row n)
+  (floor (/ (- (sqrt (+ 1 (* 8 n))) 1) 2)))
+
+; Now we can calulate pascal's triangle with a single parameter of n.
+
+(define (pascaln n)
+  (cond ((< n 0) 0) ; Out of bounds
+        ((= n 0) 1) ; Base case
+        ((or (triangle-number? n) (triangle-number? (inc n))) 1)
+        (else (+ (pascaln (- n (pascal-row n) 1))
+                 (pascaln (- n (pascal-row n)))))))
+    
+  
+    
+
+      
