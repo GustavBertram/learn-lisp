2b628ce70d7ba6cbe28369f9e69d2e6abd4c8f44
Gustav Bertram <gustav.bertram@gmail.com>
2018-05-19 22:38:02 +0200

    Add to Land of Lisp Ch 18

---
+++ land_of_lisp/18_doom_dice_v2.lisp
@@ -0,0 +1,54 @@
+;;;; DICE OF DOOM V2
+;;; Contains code from Land of Lisp Ch 18.
+;;; Updating Dice of Doom with lazy evaluation.
+
+(load "15_doom_dice.lisp")
+(load "18_lazy_evaluation.lisp")
+
+;;; Bigger board
+(defparameter *board-size* 4)
+(defparameter *board-hexnum* (* *board-size* *board-size*))
+
+;;; Redefine game functions using lazy library
+
+(defun add-passing-move (board player spare-dice first-move moves)
+  (if first-move
+      moves
+      (lazy-cons (list nil
+                       (game-tree (add-new-dice board player
+                                                (1- spare-dice))
+                                  (mod (1+ player) *num-players*)
+                                  Lazy Programming 385
+                                  0
+                                  t))
+                 moves)))
+
+(defun attacking-moves (board cur-player spare-dice)
+  (labels ((player (pos)
+             (car (aref board pos)))
+           (dice (pos)
+             (cadr (aref board pos))))
+    (lazy-mapcan
+     (lambda (src)
+       (if (eq (player src) cur-player)
+           (lazy-mapcan
+            (lambda (dst)
+              (if (and (not (eq (player dst)
+                                cur-player))
+                       (> (dice src) (dice dst)))
+                  (make-lazy
+                   (list (list (list src dst)
+                               (game-tree (board-attack board
+                                                        cur-player
+                                                        src
+                                                        dst
+                                                        (dice src))
+                                          cur-player
+                                          (+ spare-dice (dice dst))
+                                          nil))))
+                  (lazy-nil)))
+            (make-lazy (neighbors src)))
+           (lazy-nil)))
+     (make-lazy (loop for n below *board-hexnum*
+                      collect n)))))
+
